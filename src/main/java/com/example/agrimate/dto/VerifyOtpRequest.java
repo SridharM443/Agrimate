cfe1ad1ea6e63bb0c30050ebc63dc7025313ae6d
SridharM443 <122192712+SridharM443@users.noreply.github.com>
2025-10-01 14:40:06 +0530

    Delete src directory

--- src/main/java/com/example/agrimate/dto/VerifyOtpRequest.java
+++
@@ -1,8 +0,0 @@
-package com.example.agrimate.dto;
-
-import jakarta.validation.constraints.*;
-
-public record VerifyOtpRequest(
-        @Email @NotBlank String email,
-        @NotBlank String code
-) {}
--- src/main/java/com/example/agrimate/repository/CropPredictionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.agrimate.repository;
-
-import com.example.agrimate.entity.CropPrediction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CropPredictionRepository extends JpaRepository<CropPrediction, Long> {}
